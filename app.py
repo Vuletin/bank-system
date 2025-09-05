67c8a086ff1c91ad8fc1668ddb17c68fc5b07284
Sava Vuletin <vuletin92@gmail.com>
2025-09-05 14:30:27 +0200

    db url debug

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    
+    print("DATABASE_URL at startup:", os.getenv("DATABASE_URL"))
 
     # Configuration
     app = Flask(__name__)
